8a6637bbd328ee4e5b66a8db219c6955eadf8efb
altavada <117209509+altavada@users.noreply.github.com>
2022-12-30 19:19:47 -0500

    time-up window alert and quiz-replay button

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -3,6 +3,7 @@
 // target elements stored as global variables for easy, open reference
 var startButton = document.getElementById("startbutton");
 var nextButton = document.getElementById("nextquestion");
+var replay = document.getElementById("replay");
 var minuteHand = document.getElementById("minutehand");
 var secondHand = document.getElementById("secondhand");
 var quizBox = document.getElementById("quizbox");
@@ -32,7 +33,7 @@ var allQuestions = [];
 // object containing each question, its set of answer choices, and its answer key, stored as objects
 var questions = {
     question0: {
-        q: "Which of the following is NOT a primitive data type in JavaScript?",
+        q: "1. Which of the following is NOT a primitive data type in JavaScript?",
         a: "Boolean",
         b: "Object",
         c: "Undefined",
@@ -40,7 +41,7 @@ var questions = {
         r: "b"
     },
     question1: {
-        q: 'Which of the following most accurately describes "shadowing?"',
+        q: '2. Which of the following most accurately describes "shadowing?"',
         a: "When the execution of two functions conflicts; the first function called overrides the second.",
         b: "When the execution of two functions conflicts; the second function called overrides the first.",
         c: "When a global and local variable share the same name; the function will read the local variable and ignore the global one.",
@@ -48,7 +49,7 @@ var questions = {
         r: "c"
     },
     question2: {
-        q: "What does the push() method do?",
+        q: "3. What does the push() method do?",
         a: "Adds one or more items to the end of an array and returns the new array length.",
         b: "Prioritizes the execution of a called function and passes up its return value.",
         c: "Adds a string or node object after the last child of a targeted HTML element.",
@@ -56,15 +57,15 @@ var questions = {
         r: "a",
     },
     question3: {
-        q: "An object can contain all of the following data types EXCEPT:",
+        q: "4. An object can contain each of the following EXCEPT:",
         a: "Arrays",
         b: "Objects",
         c: "Methods",
-        d: "None of the above",
+        d: "None; an object can contain all of the above",
         r: "d",
     },
     question4: {
-        q: 'A JavaScript object contains data sorted and stored as "properties." Another name for these properties is:',
+        q: '5. A JavaScript object contains data sorted and stored as "properties." Another name for these properties is:',
         a: "Elements",
         b: "Attributes",
         c: "Variables",
@@ -72,7 +73,7 @@ var questions = {
         r: "d"
     },
     question5: {
-        q: "What does an event listener do?",
+        q: "6. What does an event listener do?",
         a: "Defines a certain function to listen for and an output to display on the webpage when the function is executed.",
         b: "Defines a certain user input on the webpage and a function to execute when that input is detected.",
         c: "Defines a certain user input on the webpage and data to save to local storage when that input is detected.",
@@ -80,7 +81,7 @@ var questions = {
         r: "b"
     },
     question6: {
-        q: "Describe the functionality of the following variable: 'var futureBookings = document.body.children[2].children[1];'",
+        q: "7. Describe the functionality of the following variable: 'var futureBookings = document.body.children[2].children[1];'",
         a: "Targets the first child of the second child of the body element.",
         b: "Targets the second child of the third child of the body element.",
         c: "Targets the first child of the second child of the document element.",
@@ -88,7 +89,7 @@ var questions = {
         r: "b"
     },
     question7: {
-        q: "We can convert a JS object to a string using the JSON.stringify() method. What method can we use to convert this data back into an object format?",
+        q: "8. We can convert a JS object to a string using the JSON.stringify() method. What method can we use to convert this data back into an object format?",
         a: "JSON.trim()",
         b: "JSON.split()",
         c: "JSON.parse()",
@@ -96,7 +97,7 @@ var questions = {
         r: "c"
     },
     question8: {
-        q: 'When used inside of a defined object, the keyword "this" refers to that object. When used in a browser outside of such an object, to what does it refer?',
+        q: '9. When used inside of a defined object, the keyword "this" refers to that object. When used in a browser outside of such an object, to what does it refer?',
         a: "The Window object",
         b: "The Document object",
         c: "The JavaScript file",
@@ -104,7 +105,7 @@ var questions = {
         r: "a"
     },
     question9: {
-        q: "Let's say you need to resolve an event-bubbling issue in your JS code. What method would you use to acheive this?",
+        q: "10. Let's say you need to resolve an event-bubbling issue in your JS code. What method would you use to acheive this?",
         a: "setAttribute()",
         b: "preventDefault()",
         c: "stopPropogation()",
@@ -121,24 +122,21 @@ for (var quizItem in questions) {
 
     // EVENT LISTENERS
 
-// initiates quiz when start button is pressed, sets event listener to evaluate answers when selected
-startButton.addEventListener("click", function() {
-    quizBox.addEventListener("click", function(event) {
-        let selection = event.target;
-        if (selection.matches("p")) {
-            let answer = selection.getAttribute("data-choice");
-            if (answer === allQuestions[onQuestion].r) {
-                thisAnswer = true;
-            }
+// runs script to set up and begin quiz when start button is clicked
+startButton.addEventListener("click", quizInit);
+
+// sets event listener to evaluate answers when selected
+quizBox.addEventListener("click", function(event) {
+    let selection = event.target;
+    if (selection.matches("p")) {
+        let answer = selection.getAttribute("data-choice");
+        if (answer === allQuestions[onQuestion].r) {
+            thisAnswer = true;
         }
-    })
-    nextButton.setAttribute("style", "display:inline-block");
-    quizBox.setAttribute("style", "display:block");
-    nextQuestion();
-    countDown();
+    }
 })
 
-// records answer and cues next question when next button is clicked, or cues the results screen if quiz is completed. Subtracts 30 seconds from timer if wrong answer is detected.
+// records answer and cues next question when next button is clicked, or cues the results screen if quiz is completed. -30 second time penalty for wrong answers.
 nextButton.addEventListener("click", function() {
     if (thisAnswer === true) {
         score++;
@@ -146,16 +144,9 @@ nextButton.addEventListener("click", function() {
         let newTime = secondsLeft - 30;
         if (newTime < 0) {
             minutesLeft--;
-            minuteHand.textContent = minutesLeft;
             secondsLeft = newTime + 60;
-            if (secondsLeft <= 9) {
-                secondHand.textContent = "0" + secondsLeft;
-            } else {
-                secondHand.textContent = secondsLeft;
-            }
         } else {
             secondsLeft = newTime;
-            secondHand.textContent = secondsLeft;
         }
     }
     onQuestion++;
@@ -165,6 +156,17 @@ nextButton.addEventListener("click", function() {
     } else {
         nextQuestion();
     }
+})
+
+// replay button 
+replay.addEventListener("click", function() {
+    thisAnswer = false;
+    onQuestion = 0;
+    score = 0;
+    secondsLeft = 0;
+    minutesLeft = 5;
+    resultBox.setAttribute("style", "display:none");
+    quizInit(); 
 })
 
 // saves initials and score when save button is clicked, and adds it to the scoreboard
@@ -188,10 +190,18 @@ saveButton.addEventListener("click", function(event) {
 
     // FUNCTIONS
 
-// timer function, with time-out consequence
+function quizInit() {
+    nextButton.setAttribute("style", "display:inline-block");
+    quizBox.setAttribute("style", "display:block");
+    nextQuestion();
+    countDown();
+}
+
+// timer function (minutes + seconds), with time-out consequence
 function countDown() {
     let timeLeft = setInterval(function() {
         secondsLeft--;
+        minuteHand.textContent = minutesLeft;
         if (secondsLeft <= 9) {
             secondHand.textContent = "0" + secondsLeft;
         }
@@ -206,6 +216,9 @@ function countDown() {
         }
         if ((minutesLeft === 0 && secondsLeft === 0) || minutesLeft < 0) {
             clearInterval(timeLeft);
+            window.alert("Time's up!");
+            minuteHand.textContent = "0";
+            secondHand.textContent = "00";
             showScore();
         } else if (onQuestion === allQuestions.length) {
             clearInterval(timeLeft);
